f4e85b4126e1aadd1f7072dcf29f792352895693
Scruffy pc <cayabyabrr@yahoo.com>
2024-03-09 00:55:49 -0800

    example scm

---
+++ monsoonami.scm
@@ -0,0 +1,18 @@
+(define (script-fu-smooth-threshhold image layer)
+  (gimp-threshold layer 127 255)
+  (plug-in-gauss RUN_NONINTERACTIVE image layer 3 3 1)
+  (gimp-levels layer HISTOGRAM-VALUE 95 160 1.0 0 255)
+  (gimp-displays-flush)
+)
+
+(script-fu-register
+  "script-fu-smooth-threshold"
+  "<Image>/Colors/Smooth Threshold"
+  "Threshold with smooth edges."
+  "monsoonami"
+  "monsoonami"
+  "June 2012"
+  "RGB*, GRAY*"
+  SF-IMAGE      "Image" 0
+  SF-DRAWABLE   "Layer" 0
+)
